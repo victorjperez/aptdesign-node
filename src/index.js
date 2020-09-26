0fbf56354217d8d3dc7588ccf97926d8273ef5ff
Victor Perez <vjperez@mix.wvu.edu>
2020-09-26 11:26:09 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const { GraphQLServer } = require("graphql-yoga")
+const { PrismaClient } = require("@prisma/client");
+const { PubSub } = require("graphql-yoga");
+// Files
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const Subscription = require("./resolvers/Subscription");
+const User = require("./resolvers/User");
+const Link = require("./resolvers/Link");
+
+const resolvers = {
+  Query,
+  Mutation,
+  Subscription,
+  User,
+  Link,
+};
+const pubsub = new PubSub();
+const prisma = new PrismaClient();
+
+const server = new GraphQLServer({
+  typeDefs: "./src/schema.graphql",
+  resolvers,
+  context: (request) => {
+    return {
+      ...request,
+      prisma,
+      pubsub,
+    };
+  },
+});
+server.start(() => console.log(`Server is running on http://localhost:4000`));
